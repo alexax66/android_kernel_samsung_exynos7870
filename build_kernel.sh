19edc33d832526132ba42ff97f9288f9d4e0bb2c
Alexander Alexeev <alexax66@gmail.com>
2020-05-23 18:11:23 +0300

    Rework build_kernel.sh and fix dbt.img building
     # Пожалуйста, введите сообщение коммита для ваших изменений. Строки,

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=~/android/kernel/toolchain/gcc-linaro-7.4.1/bin/aarch64-linux-gnu-
+export CROSS_COMPILE=~/build/toolchain/aarch64-cortex_a53-linux-gnueabi-7.4.0/bin/aarch64-cortex_a53-linux-gnu-
 export ANDROID_MAJOR_VERSION=p
 
-make O=./out a3y17ltexc_00_defconfig
-make O=./out -j16
+THREAD=-j$(bc <<< $(grep -c ^processor /proc/cpuinfo)+2)
+DTBH_PLATFORM_CODE=0x50a6
+DTBH_SUBTYPE_CODE=0x217584da
+
+
+make O=./out $1
+make O=./out $THREAD
+
+$(pwd)/dtbTool  -o "$(pwd)/out/arch/arm64/boot/dtb.img" -s 2048 -d "$(pwd)/out/arch/arm64/boot/dts/" --platform $DTBH_PLATFORM_CODE --subtype $DTBH_SUBTYPE_CODE
+
+cp out/arch/arm64/boot/Image  out/arch/arm64/boot/recovery.img-kernel
+cp out/arch/arm64/boot/dtb.img  out/arch/arm64/boot/recovery.img-dt
