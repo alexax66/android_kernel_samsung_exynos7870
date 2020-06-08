90dc7bf1c70a47e819cbbe6c20bbfbd3f9b147ee
Samuel Pascua <sgpascua@ngcp.ph>
2020-06-08 12:36:05 +0800

    selinux: disable audit
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2924,6 +2924,7 @@ static noinline int audit_inode_permission(struct inode *inode,
 					   int result,
 					   unsigned flags)
 {
+#ifdef CONFIG_AUDIT
 	struct common_audit_data ad;
 	struct inode_security_struct *isec = inode->i_security;
 	int rc;
@@ -2935,6 +2936,7 @@ static noinline int audit_inode_permission(struct inode *inode,
 			    audited, denied, result, &ad, flags);
 	if (rc)
 		return rc;
+#endif
 	return 0;
 }
 
