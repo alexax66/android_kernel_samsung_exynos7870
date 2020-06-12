f25a3ca197547fdd2eeee1bf3cc0501e844a36fc
Alexander Alexeev <alexax66@gmail.com>
2020-06-12 18:46:00 +0300

    fix sdfat and readahead buid

--- fs/sdfat/blkdev.c
+++ fs/sdfat/blkdev.c
@@ -51,10 +51,10 @@
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 0, 0)
 	/* EMPTY */
 #else /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 0, 0) */
-static struct backing_dev_info *inode_to_bdi(struct inode *bd_inode)
-{
-	return bd_inode->i_mapping->backing_dev_info;
-}
+//static struct backing_dev_info *inode_to_bdi(struct inode *bd_inode)
+//{
+//	return bd_inode->i_mapping->backing_dev_info;
+//}
 #endif
 
 /*======================================================================*/
